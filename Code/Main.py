bfd1532872fca5c85ff23ccb2a0b82379a201d34
victor macedo <victormacedo996@gmail.com>
2021-01-06 18:10:52 -0300

    updated files

--- Code/Main.py
+++ Code/Main.py
@@ -6,23 +6,26 @@ from Menus import mainMenu, waterMenu, wheatFlourMenu, honeyMenu
 while True:
     mainMenu()
     food_choice = getInteger ('Choose the food for analisys: ')
-
-    if food_choice == 1:
-        wheatFlourMenu ()
-       
-    elif food_choice == 2:
-        honeyMenu ()
-        
-    elif food_choice == 3:
-        sucroseMenu ()
-        
-    elif food_choice == 4:
-        waterMenu ()
-        
+    if food_choice > 4 or food_choice < 0:
+        print('Invalid option')
+        sleep(1)
     else:
-        print('Thank you for using the program')
-        sleep(1.5)
-        exit()
+        if food_choice == 1:
+            wheatFlourMenu ()
+        
+        elif food_choice == 2:
+            honeyMenu ()
+            
+        elif food_choice == 3:
+            sucroseMenu ()
+            
+        elif food_choice == 4:
+            waterMenu ()
+            
+        else:
+            print('Thank you for using the program')
+            sleep(1.5)
+            exit()
 
 
 
